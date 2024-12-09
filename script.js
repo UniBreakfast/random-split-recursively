23cb6b648cee83d725caa7646647d130f7225db6
UniBreakfast <unibreakfast1@gmail.com>
2024-12-09 16:25:35 +0200

    remove on click and fill empty places on idle

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
+const duration = 1000;
 const maxDepth = undefined //|| 2;
 const lineWidth = 6;
 const minSize = lineWidth * 4;
 
+let pause = false;
+let timerId;
+
 body.style.setProperty('--line', `${lineWidth}px`);
+body.style.setProperty('--time', `${duration}ms`);
 
 splitRecursively(body, maxDepth);
+
+body.onclick = handleClick;
+
+function handleClick(e) {
+  if (e.target === body) return;
+  
+  const parent = e.target.parentElement;
+
+  pause = true;
+
+  clearTimeout(timerId);
+
+  timerId = setTimeout(() => {
+    pause = false;
+
+    const leaves = document.querySelectorAll(':empty');
+console.log(leaves);
+    for (const leave of leaves) {
+      splitRecursively(leave);
+    }
+  }, duration * 3);
+  
+  for (el of parent.children) {
+    el.classList.add('transparent');
+  }
+  setTimeout(() => parent.replaceChildren(), duration);
+}
 
 function splitRecursively(el, depth = Infinity) {
   if (depth <= 0) return;
@@ -17,14 +49,24 @@ function splitRecursively(el, depth = Infinity) {
   const willSplit = longSide > minSize * 2 + gap
     && shortSide > minSize;
 
-  el.dataset.sizes = `longside ${longSide}px shortside ${shortSide}px`;
-
   if (willSplit) {
     if (vertical) splitVertically(el, width - gap);
     else splitHorizontally(el, height - gap);
 
     for (const child of el.children) {
-      splitRecursively(child, depth - 1);
+      child.classList.add('transparent');
+      
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          child.classList.remove('transparent')
+  
+          setTimeout(() => {
+            if (pause) return;
+
+            splitRecursively(child, depth - 1);
+          }, duration);
+        });
+      });
     }
   }
 }
@@ -32,7 +74,6 @@ function splitRecursively(el, depth = Infinity) {
 function splitVertically(el, width) {
   const child1 = document.createElement('div');
   const child2 = document.createElement('div');
-  // const width1 = width * 0.5;
   const maxWidth = width - 2 * minSize;
   const width1 = Math.floor(Math.random() * maxWidth) + minSize;
   const width2 = width - width1;
@@ -48,7 +89,6 @@ function splitVertically(el, width) {
 function splitHorizontally(el, height) {
   const child1 = document.createElement('div');
   const child2 = document.createElement('div');
-  // const height1 = height * 0.5;
   const maxHeight = height - 2 * minSize;
   const height1 = Math.floor(Math.random() * maxHeight) + minSize;
   const height2 = height - height1;
@@ -65,10 +105,10 @@ function getContentSize(el) {
     paddingTop, paddingRight, paddingBottom, paddingLeft
   } = getComputedStyle(el);
 
-  const width = el.clientWidth 
+  const width = el.clientWidth
     - parseFloat(paddingLeft) - parseFloat(paddingRight);
 
-  const height = el.clientHeight 
+  const height = el.clientHeight
     - parseFloat(paddingTop) - parseFloat(paddingBottom);
 
   return { width, height };
